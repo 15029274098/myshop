bd0ae709b591cc3d7483187bd919ff532d0972f4
15029274098 <15029274098@163.com>
2019-11-16 23:43:10 +0800

    webpack打包处理

---
+++ mmail-fe/src/page/common/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2019-11-16 13:05:41
+ * @LastEditTime: 2019-11-16 13:23:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \mmail-fe\src\common\index.js
+ */
+'use strict'
+console.log('i am gobal');
---
+++ mmail-fe/src/page/login/login.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: mikey.zhaopeng 
+ * @Date: 2019-11-16 10:04:47 
+ * @Last Modified by:   mikey.zhaopeng 
+ * @Last Modified time: 2019-11-16 10:04:47 
+ */
+'use strict'
+var $ = require('jquery');
+require('../module.js');
+require('./login.css');
+$(function () {
+    $('#btn').text('登陆成功');
+});
+
+ 
