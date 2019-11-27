fc199db4f7af3181ee17224fd5153681aacdca8f
15029274098 <15029274098@163.com>
2019-11-27 22:58:27 +0800

    修改前端文件

--- mmail-fe/src/page/index/index.js
+++ mmail-fe/src/page/index/index.js
@@ -1,20 +1,19 @@
 /*
  * @Author: your name
  * @Date: 2019-11-16 08:53:24
- * @LastEditTime: 2019-11-16 14:27:15
+ * @LastEditTime: 2019-11-24 23:29:32
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Editole
  * @FilePath: \mmail-fe\src\page\index\index.js
  */
 
 'use strict'
-var $ = require('jquery');
+var _mm = require('util/mm.js');
 require('./index.css');
-require('../module.js');
-console.log('hello world');
-$(function () {
-    $('p').text('测试');
-    $("#btn2").click(function () {
-        $("ol").prepend("<li>在开头添加列表项</li>");
-    });
-});
+require('../common/layout.css');
+require('node_modules/font-awesome/css/font-awesome.min.css');
+var html = '<div>{{data}}</div>';
+var data = {
+    data: 123
+}
+console.log(_mm.renderHtml(html, data));
